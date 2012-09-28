f8ae7da1b4937e3863206eb5edd9b87020d60e37
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-09-28 15:35:07 +0200

    Email field is now mandatory in the user database

--- server/Server.rb
+++ server/Server.rb
@@ -3,6 +3,9 @@
 require 'eventmachine'
 require 'evma_httpserver'
 
+load "Constant.rb"
+load "Protocol.rb"
+
 class Server < EM::Connection
   include EM::HttpServer
 
@@ -13,24 +16,51 @@ class Server < EM::Connection
 
   def process_http_request
     response = EM::DelegatedHttpResponse.new self
-    response.status = 200
-    response.content_type 'text/html'
-    response.content = description_kikoo
+    response.content_type 'text/plain'
+    response.content = getResponse
     response.send_response
   end
 
-  def description_kikoo 
-    res = "http_protocol => #{@http_protocol}\n<br/>\n"
-    res += "http_request_method => #{@http_request_method}\n<br/>\n"
-    res += "http_cookie => #{@http_cookie}\n<br/>\n"
-    res += "http_if_none_match => #{@http_if_none_match}\n<br/>\n"
-    res += "http_content_type => #{@http_content_type}\n<br/>\n"
-    res += "http_path_info => #{@http_path_info}\n<br/>\n"
-    res += "http_request_uri => #{@http_request_uri}\n<br/>\n"
-    res += "http_query_string => #{@http_query_string}\n<br/>\n"
-    res += "http_post_content => #{@http_post_content}\n<br/>\n"
-    res += "http_headers => #{@http_headers}\n<br/>\n"
+  private
+
+  def getDebug
+    res = "http_protocol => #{@http_protocol}\n"
+    res += "http_request_method => #{@http_request_method}\n"
+    res += "http_cookie => #{@http_cookie}\n"
+    res += "http_if_none_match => #{@http_if_none_match}\n"
+    res += "http_content_type => #{@http_content_type}\n"
+    res += "http_path_info => #{@http_path_info}\n"
+    res += "http_request_uri => #{@http_request_uri}\n"
+    res += "http_query_string => #{@http_query_string}\n"
+    res += "http_post_content => #{@http_post_content}\n"
+    res += "http_headers => #{@http_headers}\n"
     return res
+  end
+
+  def getResponse
+    return Constant::Fail if unvalidRequest?
+    return Constant::Fail if unknownRequest?
+    
+  end
+
+  def unvalidRequest?
+    if http_protocol != "HTTP/1.1"
+      or http_request_method != "POST"
+      or http_path_info != "/mine/protocol/request"
+      or http_content_type != "application/x-www-form-urlencoded"
+      response.status = Constant::UnvalidRequest
+      return true
+    end
+    return true if (http_post_content =~ /.+={.*}/) == 0
+    false
+  end
+
+  def unknownRequest?
+    unless Protocol::Requests.index http_post_content.split('=')[0]
+      response.status = Constant::UnknownRequest
+      return true
+    end
+    false
   end
 end
 
