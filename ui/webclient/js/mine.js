7fa26665c98c1585bb9490d9fd1da29fa4b8a265
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-10 10:13:11 +0200

    Keycodes shown when a key is pressed [TEST]

--- ui/webclient/js/mine.js
+++ ui/webclient/js/mine.js
@@ -5,5 +5,11 @@ $(function() {
 	var pass = $('#pass').val();
 	$('#pass').val('');
 	alert(name + ' : ' + pass + ' were sent');
-    })
+    });
+    $(document).keydown(function(e) {
+	$('#char').text(e.which);
+	if (e.which == 191 || e.which == 190) {
+	    e.preventDefault();
+	}
+    });
 });
