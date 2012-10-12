e85aec625e81b5e25cd4d57835ce54cd64aa00d1
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-12 11:15:24 +0200

    Added Rails WebClient

---
+++ ui/webclient/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webclient::Application.config.secret_token = 'b8d66d29964d0df7af68f9a8b343985145af7452519d30b43bb8dbb2e56510ded227152203c778598924eb0a7f24fe3c3f6c5113516974900a74eff5bbabc7be'
