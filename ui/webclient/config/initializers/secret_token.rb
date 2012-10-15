b5bd77ecb81940e0aff4a8ad595671a002c122ff
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-15 11:13:00 +0200

    Deleted Test::Unit framework use

--- ui/webclient/config/initializers/secret_token.rb
+++ ui/webclient/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Webclient::Application.config.secret_token = 'b8d66d29964d0df7af68f9a8b343985145af7452519d30b43bb8dbb2e56510ded227152203c778598924eb0a7f24fe3c3f6c5113516974900a74eff5bbabc7be'
+Webclient::Application.config.secret_token = '4d15a804b4840e1b11595a72896f5736c39cfe01ceaaf5c4812753b4f3ebdceb0b47fc0d8f0b434344220901e6bb7620907601a7d200587ed006e0f62417fbb3'
