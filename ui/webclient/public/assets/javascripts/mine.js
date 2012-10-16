562c0f8e2c9718d21bb44d4be3d7915dacbaf55a
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-16 10:51:25 +0200

    Deleted all public non-standard content

--- ui/webclient/public/assets/javascripts/mine.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#send').click(function() {
-	var name = $('#name').val();
-	$('#name').val('');
-	var pass = $('#pass').val();
-	$('#pass').val('');
-	alert(name + ' : ' + pass + ' were sent');
-    })
-});
