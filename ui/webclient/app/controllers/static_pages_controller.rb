a9dad06e92c1d90e4ab506fa6c71ba950d2ddabf
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-18 09:40:21 +0200

    Added helper

--- ui/webclient/app/controllers/static_pages_controller.rb
+++ ui/webclient/app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @homeTitle = "Mine Is Not Emacs : Embedded Client"
   end
 end
--- ui/webclient/app/helpers/static_pages_helper.rb
+++ ui/webclient/app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def title
+    base_title = "Mine Is Not Emacs : Embedded Client"
+    if @title
+      return "#{base_title} - #{@title}"
+    end
+    base_title
+  end
 end
