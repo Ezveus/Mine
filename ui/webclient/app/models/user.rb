0b95f6a0bebd59cb06b7735e5d90026bee54242a
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-29 11:15:51 +0100

    User model validates length of name field

--- ui/webclient/app/models/user.rb
+++ ui/webclient/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
-  validates :name, :presence => true
+  validates(:name,
+            :presence => true,
+            :length => { :maximum => 50 })
   validates :email, :presence => true
 end
--- ui/webclient/spec/models/user_spec.rb
+++ ui/webclient/spec/models/user_spec.rb
@@ -10,10 +10,17 @@ describe User do
   it "should create a new instance with valids attributes" do
     User.create!(@attr)
   end
+
   describe "Name Tests" do
     it "should need a name" do
       bad_guy = User.new(@attr.merge(:name => ""))
       bad_guy.should_not be_valid
+    end
+
+    it "should reject too long names" do
+      long_name = "a" * 51
+      long_name_user = User.new(@attr.merge(:name => long_name))
+      long_name_user.should_not be_valid
     end
   end
 
