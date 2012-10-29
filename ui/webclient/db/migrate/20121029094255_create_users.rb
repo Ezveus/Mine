cd3cd5fedff0a9c1da3fd307abf600169dc59097
Matthieu "Ezveus" Ciappara <ciappam@gmail.com>
2012-10-29 14:13:05 +0100

    Database has indexes on name and email safeguarding uniqueness

--- ui/webclient/db/migrate/20121029094255_create_users.rb
+++ ui/webclient/db/migrate/20121029094255_create_users.rb
@@ -1,4 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
+  def up
+    change
+  end
+
   def change
     create_table :users do |t|
       t.string :name
@@ -6,5 +10,9 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :users
   end
 end
--- ui/webclient/db/schema.rb
+++ ui/webclient/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121029094255) do
+ActiveRecord::Schema.define(:version => 20121029130521) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20121029094255) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
