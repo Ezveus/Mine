e91b47f013dc81042f9f5b609b69a772df06461b
Olivier Dumenil <olivier.dumenil@epitech.eu>
2013-02-20 18:37:32 +0100

    added possibility to create TCPServers, WebSocketServers

--- lib/Mine/Server/Socket.rb
+++ lib/Mine/Server/Socket.rb
@@ -1,12 +1,12 @@
 module Mine
   module Socket
     def self.create rhost, rport, type = :tcp
-      if type == :tcp
-        Celluloid::IO::TCPSocket.new rhost, rport
-      elsif type == :wsp
-        WEBSocket::Socket.new rhost, rport
-      else
-        Celluloid::IO::TCPSocket.new rhost, rport
+      case type
+      when :tcp then Celluloid::IO::TCPSocket.new rhost, rport
+      when :wsp then WEBSocket::Socket.new rhost, rport
+      when :tcps then Celluloid::IO::TCPServer.new rhost, rport
+      when :wsps then WEBSocket::Server.new rhost, rport
+      else raise "Unknown socket type"
       end
     end
   end
